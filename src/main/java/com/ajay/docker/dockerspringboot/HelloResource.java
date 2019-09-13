b189f2952d3a64f2e28736268cb20c542305f625
Ajay Desai <ajay.adesai@tcs.com>
2019-09-12 21:16:36 -0700

    first commit

---
+++ src/main/java/com/ajay/docker/dockerspringboot/HelloResource.java
@@ -0,0 +1,16 @@
+package com.ajay.docker.dockerspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class HelloResource {
+
+    @GetMapping
+    public String hello(){
+        System.out.println("just saying hi!");
+        return "Hello World!";
+    }
+}
