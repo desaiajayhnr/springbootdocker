3cdb5185bdba77d81667383b1f5e50bc4124df2b
desaiajayhnr <38744856+desaiajayhnr@users.noreply.github.com>
2019-09-17 15:33:47 -0700

    Updated the hello world

--- src/main/java/com/ajay/docker/dockerspringboot/HelloResource.java
+++ src/main/java/com/ajay/docker/dockerspringboot/HelloResource.java
@@ -11,6 +11,6 @@ public class HelloResource {
     @GetMapping
     public String hello(){
         System.out.println("just saying hi!");
-        return "Hello World!";
+        return "Hello Shankar!";
     }
 }
